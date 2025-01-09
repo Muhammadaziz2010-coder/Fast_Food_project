191db40ab16d539959b50c8b57d9cc304a5fb612
Muhammadaziz2010-coder <muhammadazizabdulvosiyev638@gmail.com>
2025-01-09 16:53:41 +0500

    first commit

---
+++ app/Repositories/Measurement/MeasurementRepositoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repositories\Measurment;
+
+use Prettus\Repository\Contracts\RepositoryInterface;
+
+/**
+ * Interface MeasurementRepositoryRepository.
+ *
+ * @package namespace App\Repositories\Measurement;
+ */
+interface MeasurementRepositoryInterface extends RepositoryInterface
+{
+
+}
